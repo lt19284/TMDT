04b21d1a2bfd20a039948cd2b7e15f098af5f60c
lt19284 <143415584+lt19284@users.noreply.github.com>
2024-11-30 09:54:41 +0700

    Update app.py

--- app.py
+++ app.py
@@ -181,6 +181,6 @@ def add_product():
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 10080))
-    app.run(host="127.0.0.1",port=port)
+    app.run(host="0.0.0.0",port=port)
     app.run(debug=True)
 
